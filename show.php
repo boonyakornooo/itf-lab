8ac165c749f3c10a784c1d1928a52c17709302db
boonyakornooo <73694672+boonyakornooo@users.noreply.github.com>
2020-11-13 17:51:44 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'itf-data.mysql.database.azure.com', 'it63070235@itf-data', 'NFMkwk23', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
