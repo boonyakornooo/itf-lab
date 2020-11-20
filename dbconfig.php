8e15124d68077981ebcf3d5f3fdbeaec18510840
boonyakornooo <73694672+boonyakornooo@users.noreply.github.com>
2020-11-20 15:43:50 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'host' => 'host',
-    'username' => 'username',
-    'password' => 'password',
-    'database' => 'databasename',
+    'host' => 'itf-data.mysql.database.azure.com',
+    'username' => 'it63070235@itf-data',
+    'password' => 'NFMkwk23',
+    'database' => 'ITFLab',
 );
 ?>
